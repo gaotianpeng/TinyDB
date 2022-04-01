6409fcd33402754bd41ec9bc5729d3eaeaa55eab
gaotianpeng <gtpsydm@gmail.com>
2022-04-01 19:53:42 +0800

    daily practise

---
+++ src/ut/SoundTest.cc
@@ -0,0 +1,18 @@
+#include "gmock/gmock.h"
+
+/*
+    {测试用例名称, 测试的描述性名称}
+    测试用例(test case): 是一些能共享数据和子程序的测试集合
+*/
+
+class Soundex {
+public:
+    std::string encode(const std::string& word) const {
+        return "";
+    }
+};
+
+TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
+    Soundex soundex;
+    auto encoded = soundex.encode("A");
+}
